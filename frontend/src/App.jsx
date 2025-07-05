d413ae0a45fdd9d4f8bbf2fff5e03aab836650b0
Sarat20 <saratkumarkalagara2398s@gmail.com>
2025-07-05 11:56:25 +0530

    added login and sign up part and integrated with navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,22 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import {ToastContainer,toast} from 'react-toastify'
-import Header from './components/Header'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Login from './pages/Login'
+import Home from './pages/Home'
+
 const App = () => {
   return (
     <div className="mx-10 sm:mx-[10%]">
+      <ToastContainer/>
      <Navbar/>
-      <Header/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+
+      </Routes>
+      
       <Footer/>
     </div>
   )
