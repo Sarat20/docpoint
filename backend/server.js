393862cc217d27954f6c9d8cd93f6b0552fad0b2
Sarat Kumar Kalagara <saratkumarkalagara2398s@gmail.com>
2025-07-18 21:34:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,8 @@ connectDB();
 connectCloudinary();
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true            
+  origin: ['http://localhost:5173', 'https://docpoint.onrender.com'],
+  credentials: true,
 }));
 
 app.use('/api/user',userRouter)
